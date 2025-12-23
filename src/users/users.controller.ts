928e531924c9b4e496ee6445693b9a45600e79b4
nabeelnaeem <nnabeel03@gmail.com>
2025-12-23 12:44:12 +0500

    Params, Query and Body explanation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Param, Query, Body, Req } from '@nestjs/common';
+import type { Request } from 'express';
 
 @Controller('users')
 export class UsersController {
-  @Get()
-  public getUsers() {
+  // @Get('{/:id}') if we want to keep it optional
+  // This is the newer syntax of making a param optional in nest
+  @Get('/:id{/:optional}')
+  public getUsers(@Param() params: any, @Query() query: any) {
+    console.log(params);
+    console.log(query);
     return 'You send a get request to users endpoint';
   }
 
   @Post()
-  public createUsers() {
+  // public createUsers(@Body() requestBody: any) {
+  // Above is how we grab request body in nest js
+  // Below is how we grab request body in express js
+  // Only use express version when modifying req is not possible inside nest js
+  public createUsers(@Req() request: Request) {
+    console.log(request.body);
     return 'You send a post request to users endpoint';
   }
 }
