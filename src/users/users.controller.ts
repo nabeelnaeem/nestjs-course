f7886c27a93d6ea2c5b7d8aa19f737dc18dab515
nabeelnaeem <nnabeel03@gmail.com>
2025-12-23 13:48:10 +0500

    Additional Request components in nest js

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Param, Query, Body, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Query,
+  Body,
+  Req,
+  Headers,
+  Ip,
+} from '@nestjs/common';
 import type { Request } from 'express';
 
 @Controller('users')
@@ -6,7 +16,9 @@ export class UsersController {
   // @Get('{/:id}') if we want to keep it optional
   // This is the newer syntax of making a param optional in nest
   @Get('/:id{/:optional}')
-  public getUsers(@Param() params: any, @Query() query: any) {
+  // public getUsers(@Param() params: any, @Query() query: any) {
+  // Below we are only extracting id and name
+  public getUsers(@Param('id') params: any, @Query('name') query: any) {
     console.log(params);
     console.log(query);
     return 'You send a get request to users endpoint';
@@ -17,8 +29,14 @@ export class UsersController {
   // Above is how we grab request body in nest js
   // Below is how we grab request body in express js
   // Only use express version when modifying req is not possible inside nest js
-  public createUsers(@Req() request: Request) {
+  public createUsers(
+    @Req() request: Request,
+    @Headers() headers: any,
+    @Ip() ip: any,
+  ) {
     console.log(request.body);
+    console.log(headers);
+    console.log(ip);
     return 'You send a post request to users endpoint';
   }
 }
