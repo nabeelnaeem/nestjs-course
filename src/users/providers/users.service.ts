0bf1ff93e0151d814a709d13235f68292d1fa67f
nabeelnaeem <nnabeel03@gmail.com>
2025-12-27 16:54:31 +0500

    Getting started with CompoDoc: How to do code documentation

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -2,14 +2,23 @@ import { Injectable, forwardRef, Inject } from '@nestjs/common';
 import { GetUsersParamsDto } from '../dtos/get-users-params.dto';
 import { AuthService } from 'src/auth/providers/auth.service';
 
+/**
+ * Service for user domain operations; delegates auth-aware checks to AuthService.
+ */
 @Injectable() //As soon as we have this decorator, now this class became provider
 export class UsersService {
+  /**
+   * Injects AuthService (forwardRef to break circular dependency).
+   */
   constructor(
     // Injecting authService
     @Inject(forwardRef(() => AuthService))
     private readonly authService: AuthService,
   ) {}
 
+  /**
+   * Returns a list of users filtered/paginated by the supplied params; currently stubbed with sample data after an auth check.
+   */
   public findAll(
     getUsersParamsDto: GetUsersParamsDto,
     limit: number,
@@ -29,6 +38,9 @@ export class UsersService {
     ];
   }
 
+  /**
+   * Retrieves a single user by ID; currently returns a static stub.
+   */
   public findOneById(id: string) {
     return {
       id,
