1463dda31df5dcdf732ebc0b40664009a91b06e8
nabeelnaeem <nnabeel03@gmail.com>
2025-12-31 12:17:02 +0500

    Created meta-options service

---
+++ src/meta-options/providers/meta-options.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePostMetaOptionsDto } from '../dtos/create-post-meta-options.dto';
+import { Repository } from 'typeorm';
+import { MetaOption } from '../meta-option.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class MetaOptionsService {
+  constructor(
+    /**
+     * Inject metaOptionsRepository
+     */
+    @InjectRepository(MetaOption)
+    private readonly metaOptionsRepository: Repository<MetaOption>,
+  ) {}
+
+  public async create(CreatePostMetaOptionsDto: CreatePostMetaOptionsDto) {
+    const metaOption = this.metaOptionsRepository.create(
+      CreatePostMetaOptionsDto,
+    );
+    return await this.metaOptionsRepository.save(metaOption);
+  }
+}
